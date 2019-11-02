d0f1f1276e3c8bb6f092f9186189a43c459ebfe3
Joseph Miller <jdworldtv@gmail.com>
2019-11-02 05:13:27 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,8 +1,16 @@
-#using_times
-7.times do 
-puts  "Wingardium Leviousa"
+def #using_times
+
+7.times do
+  "Wingardium Leviosa"= "Wingardium Leviosa"
+  puts  "Wingardium Leviosa"
+  
+
 end
 
-
-
-
+# > Wingardium Leviosa
+# > Wingardium Leviosa
+# > Wingardium Leviosa
+# > Wingardium Leviosa
+# > Wingardium Leviosa
+# > Wingardium Leviosa
+# > Wingardium Leviosa
